67a207dc41afcc8a1f104ea299c6044e4c884a28
Marcelo Colares <marcelocolares@MacBook-Air-de-Marcelo.local>
2024-01-07 21:21:23 -0300

    insert validation on client dto

--- src/main/java/com/cwt/citywheels/controller/ClientController.java
+++ src/main/java/com/cwt/citywheels/controller/ClientController.java
@@ -2,24 +2,40 @@ package com.cwt.citywheels.controller;
 
 
 import com.cwt.citywheels.dto.ClientCreateRequest;
-import javax.validation.Valid;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
 @Slf4j
 @Validated
 @RestController
 @RequestMapping(value = "api/clients")
 public class ClientController {
 
-
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Object> create(@RequestBody ClientCreateRequest request) {
-        return ResponseEntity.ok("Cliente criado com sucesso");
+    public void  create(@RequestBody @Valid ClientCreateRequest request) {
+
+        //return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return new ResponseEntity<>(extractValidationErrorMessages(ex), HttpStatus.BAD_REQUEST);
+    }
+
+    private String extractValidationErrorMessages(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .reduce((msg1, msg2) -> msg1 + "\n" + msg2)
+                .orElse("Erro de validação desconhecido");
     }
 
 }
--- src/main/java/com/cwt/citywheels/dto/ClientCreateRequest.java
+++ src/main/java/com/cwt/citywheels/dto/ClientCreateRequest.java
@@ -1,14 +1,26 @@
 package com.cwt.citywheels.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.ToString;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+@Data
+@ToString
 public class ClientCreateRequest {
-    @NotBlank(message = "O campo nome não deve estar em branco")
+    @NotBlank(message = "O campo nome não pode ser vazio")
+    @Size(min=3,max=60, message = "O comprimento do nome informado deve estar entre {min} e {max}")
     private String name;
+
+    @Email(message = "Email inválido")
+    @Size(min=3,max=60, message = "O comprimento do email informado deve estar entre {min} e {max}")
+    private String email;
+
+    private LocalDate birthDate;
+    @CPF(message = "CPF inválido")
+    private String CPF;
 
 }
